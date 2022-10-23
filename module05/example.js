c1907b2a99615f04e6cdf256876169da3ce603d1
Isadora Silva <110069448+euaisa@users.noreply.github.com>
2022-10-22 20:54:59 -0700

    Update example.js

--- module05/example.js
+++ module05/example.js
@@ -1,13 +1,8 @@
-let table = prompt("Hi, there! Enter a number from 0 to 10 : ");
-
-if (table != null) {
-  document.getElementById("blackboard").innerHTML =
-function GetTableContent(table) {
-  var i = 1;                 // Set counter to 1
-  var msg = '<h2>Multiplication Table</h2>';              // Message  
-  while (i < 11) {
+var table = prompt("Hi, there! Enter a number from 0 to 10 : ");
+var i = 1;               
+var msg = '<h2>Multiplication Table</h2>';             
+while (i < 11) {
       msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
       i++;
-    }
-  return msg;
-}
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;

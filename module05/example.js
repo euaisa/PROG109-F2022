ec75176d5c2f0c53e76e600ccc8b639fef805923
Isadora Silva <110069448+euaisa@users.noreply.github.com>
2022-10-22 20:03:38 -0700

    Update example.js

--- module05/example.js
+++ module05/example.js
@@ -18,9 +18,9 @@ function GetValueFromUser(valueType) {
 }
 
 function GetTableContent(operator, table) {
-  var i = 1;                 // Set counter to 1
+  var i = 0;                 // Set counter to 1
   var msg = '<h2>Multiplication Table</h2>';              // Message  
-  if (operator === 'addition') {
+  if (operator === '+') {
     // Do addition
     while (i < 11) {
       msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';

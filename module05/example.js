ec6753eeb73a0bd1a86490ef98b24ad9712c8589
Isadora Silva <110069448+euaisa@users.noreply.github.com>
2022-10-22 19:47:58 -0700

    Update example.js

--- module05/example.js
+++ module05/example.js
@@ -1,5 +1,3 @@
-            // Unit of table
-
 operator = GetValueFromUser("operator");
 table = GetValueFromUser("table")
 
@@ -10,32 +8,29 @@ el.innerHTML = GetTableContent(operator, table);
 function GetValueFromUser(valueType) {
   var greetingMessage = 'Hi, there! ';
   if (valueType == "operator") {
-   var operator = "Enter + or * :";
+    greetingMessage += "Enter + or * ";
   }
   else
   {
-   var table = "Enter a number:";
+    greetingMessage += "Enter number: ";
   }
   return prompt(greetingMessage);
 }
 
 function GetTableContent(operator, table) {
-var i = 1;                 // Set counter to 1
-var msg = '<h2>Multiplication Table</h2>';             // Message
-
-if (operator === 'addition') {
-  // Do addition
-  while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
-} else {
-  // Do multiplication
-  while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
-    i++;
-  }
-}
+  var i = 1;                 // Set counter to 1
+  var msg = '<h2>Multiplication Table</h2>';              // Message  
+  if (operator === 'addition') {
+    // Do addition
+    while (i < 11) {
+      msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+      i++;
+    }
+  } else {
+    // Do multiplication
+    while (i < 11) {
+      msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+      i++;
     }
   }
   return msg;

b8b499fa8a22457ce48d38657f4bfee5429eefd9
Isadora Silva <110069448+euaisa@users.noreply.github.com>
2022-10-22 21:22:09 -0700

    Update example.js

--- module05/example.js
+++ module05/example.js
@@ -1,9 +1,9 @@
-var number = prompt('Hi, there! Enter a number between 0 and 10: ');
+var table = prompt('Hi, there! Enter a number between 0 and 10: ');
 var i = 1;                              
 
 var msg = '<h2>Multiplication Table</h2>';  
   while (i < 10) {
-      msg += i + ' x ' + number + ' = ' + (i * number) + '<br />';
+      msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
       i++;
   }
 

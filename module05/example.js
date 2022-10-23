a2226b23c0253cf229dc7cd64b007c6e0799fb42
Isadora Silva <110069448+euaisa@users.noreply.github.com>
2022-10-22 18:06:03 -0700

    Add files via upload

---
+++ module05/example.js
@@ -0,0 +1,39 @@
+var table = 3;             // Unit of table
+
+operator = GetValueFromUser("operator");
+table = GetValueFromUser("table")
+
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = GetTableContent(operator, table);
+
+function GetValueFromUser(valueType) {
+  var greetingMessage = 'Hello. How are you?';
+  if (valueType == "operator") {
+    greetingMessage += "Enter addition or multiplication";
+  }
+  else
+  {
+    greetingMessage += "Enter table";
+  }
+  return prompt(greetingMessage);
+}
+
+function GetTableContent(operator, table) {
+  var i = 1;                 // Set counter to 1
+  var msg = '';              // Message  
+  if (operator === 'addition') {
+    // Do addition
+    while (i < 11) {
+      msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+      i++;
+    }
+  } else {
+    // Do multiplication
+    while (i < 11) {
+      msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+      i++;
+    }
+  }
+  return msg;
+}

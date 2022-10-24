e58ad07b80e911948cd571042fdf198f6af7e8ea
Isadora Silva <110069448+euaisa@users.noreply.github.com>
2022-10-24 12:16:00 -0700

    Update functions.js

--- artifact03/functions.js
+++ artifact03/functions.js
@@ -6,8 +6,42 @@
 function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
 upRight(pHeight, pColorEven, pColorOdd, pSymbol);
 downRight(pHeight, pColorEven, pColorOdd, pSymbol);
+upLeft(pHeight, pColorEven, pColorOdd, pSymbol);
 
 }
+
+function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=1;i<=pHeight;i++){
+rLine +="<p>";
+ 
+ //Fill in the left side of every row with spaces or blanks
+ for(x=1; x<=pHeight-1;x++){
+  //rLine +="-";
+  
+ rLine +="<span class='space'>" + pSymbol +"</span>";
+ }
+ 
+//Create each line on the Rhombus
+for(j=x;j<=pHeight;j++){
+
+//Is the position even or odd so we change the color
+if (j%2)
+//even
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+//odd
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+}
+rLine +="</p>";
+// console.log(rLine);
+
+}
+
+document.getElementById("upLeft").innerHTML = rLine;
+}
+
 
 function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
 var rLine ="";

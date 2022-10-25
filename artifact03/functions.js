e5dc028f1fadb0e3ccf39f75034642090f75cd56
Isadora Silva <110069448+euaisa@users.noreply.github.com>
2022-10-25 10:49:05 -0700

    Update functions.js

--- artifact03/functions.js
+++ artifact03/functions.js
@@ -10,6 +10,40 @@ upLeft(pHeight, pColorEven, pColorOdd, pSymbol);
 downLeft(pHeight, pColorEven, pColorOdd, pSymbol);
 }
 
+function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=1;i<=pHeight;i++){
+rLine +="<p>";
+ 
+ //Fill in the left side of every row with spaces or blanks
+ for(x=1; x<=pHeight-i;x++){
+  //rLine +="-";
+  
+ rLine +="<span class='space'>" + pSymbol +"</span>";
+ }
+ 
+//Create each line on the Rhombus
+for(j=x;j<=pHeight;j++){
+
+//Is the position even or odd so we change the color
+if (j%2)
+//even
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+//odd
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+}
+rLine +="</p>";
+// console.log(rLine);
+
+}
+
+document.getElementById("downLeft").innerHTML = rLine;
+}
+
+
+
 function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
 var rLine ="";
 for (i=1;i<=pHeight;i++){

15bd2c40522b3d3d9f69e3c7acc012067b9b10ef
Isadora Silva <110069448+euaisa@users.noreply.github.com>
2022-10-31 21:53:44 -0700

    Update functions.js

--- modulo07/functions.js
+++ modulo07/functions.js
@@ -12,7 +12,7 @@ function Hotel(name, rooms, booked) {
 // Create two hotel objects
 var quayHotel = new Hotel('Quay', 40, 25);
 var parkHotel = new Hotel('Park', 120, 77);
-var parkHotel = new Hotel('Sunset', 86, 10);
+var sunsetHotel = new Hotel('Sunset', 86, 10);
 
 
 
@@ -26,6 +26,11 @@ var details2 = parkHotel.name + ' rooms: ';
     details2 += parkHotel.checkAvailability();
 var elHotel2 = document.getElementById('hotel2');
 elHotel2.textContent = details2;
+
+var details3 = sunsetHotel.name + ' rooms: ';
+    details3 += sunsetHotel.checkAvailability();
+var elHotel3 = document.getElementById('hotel3');
+elHotel3.textContent = details3;
 
 /* 
 NOTE: textContent does not work in IE8 or earlier

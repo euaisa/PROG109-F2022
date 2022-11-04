f72f8549564398861734139f81b2974aab6a9166
Isadora Silva <110069448+euaisa@users.noreply.github.com>
2022-11-03 22:14:17 -0700

    Updated email validation

--- art4/validator.js
+++ art4/validator.js
@@ -1,58 +1,39 @@
- //1) Create variable
 var validFirstname=false;
-
-//2) read value from HTML
 var firstname = document.getElementById("FirstName").value;
 
-//3) Do validation
 if (firstname==="null" || firstname==="" || firstname.length > 20)
     errorMessages += "<p>The first name is required and cannot be greater than 20 characters</p>";
 else
    validFirstname = true;
-
-//4) Send error message to HTML
 document.getElementById("errorMessages").innerHTML = errorMessages;
-
-//5) return status of each field
 return (validFirstname);
 
 
-//1) Create variable
-var validLastname=false;
-
-//2) read value from HTML
+var validLastname = false;
 var lastname = document.getElementById("LastName").value;
-
-//3) Do validation
 if (lastname==="null" || lastname==="" || lastname.length > 50)
     errorMessages += "<p>The last name is required and cannot be greater than 50 characters</p>";
 else
    validLastname = true;
-
-//4) Send error message to HMTL
 document.getElementById("errorMessages").innerHTML = errorMessages;
-
-//5) return status of each field
 return (validFirstname && validLastname);
 
 
+validEmail = false;
+var userEmail = document.getElementById("Email").value;
+var atpos = userEmail.indexOf("@");
+var dotpos = userEmail.lastIndexOf(".");
+if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=userEmail.length)
+    errorMessages += "<p>Invalid email</p>";
+else
+   validEmail = true;
+document.getElementById("Email").innerHTML = errorMessages;
+return (validFirstname && validLastname && validEmail);
 
 
 
 
 
-
-
-var frmvalidator = new Validator("myform");
- frmvalidator.addValidation("FirstName","req","Please enter your First Name");
- frmvalidator.addValidation("FirstName","maxlen=20",
-		"Max length for FirstName is 20");
-
- frmvalidator.addValidation("LastName","req", "Please enter your Last Name");
- frmvalidator.addValidation("LastName","maxlen=50");
-
- frmvalidator.addValidation("Email","maxlen=50");
- frmvalidator.addValidation("Email","req", "Please enter your Email");
 
  frmvalidator.addValidation("Phone","maxlen=15");
  frmvalidator.addValidation("Phone","numeric");

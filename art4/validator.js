b6d6ed5143d13a1dd79d3004bb1775b2e9358781
Isadora Silva <110069448+euaisa@users.noreply.github.com>
2022-11-03 22:08:13 -0700

    Updated Last Name Validator

--- art4/validator.js
+++ art4/validator.js
@@ -1,17 +1,44 @@
  //1) Create variable
 var validFirstname=false;
+
 //2) read value from HTML
 var firstname = document.getElementById("FirstName").value;
+
 //3) Do validation
 if (firstname==="null" || firstname==="" || firstname.length > 20)
     errorMessages += "<p>The first name is required and cannot be greater than 20 characters</p>";
 else
    validFirstname = true;
+
 //4) Send error message to HTML
 document.getElementById("errorMessages").innerHTML = errorMessages;
 
 //5) return status of each field
 return (validFirstname);
+
+
+//1) Create variable
+var validLastname=false;
+
+//2) read value from HTML
+var lastname = document.getElementById("LastName").value;
+
+//3) Do validation
+if (lastname==="null" || lastname==="" || lastname.length > 50)
+    errorMessages += "<p>The last name is required and cannot be greater than 50 characters</p>";
+else
+   validLastname = true;
+
+//4) Send error message to HMTL
+document.getElementById("errorMessages").innerHTML = errorMessages;
+
+//5) return status of each field
+return (validFirstname && validLastname);
+
+
+
+
+
 
 
 

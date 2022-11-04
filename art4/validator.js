0d35884c94323b25b7b399a1d8b1a55909a2b82e
Isadora Silva <110069448+euaisa@users.noreply.github.com>
2022-11-03 21:37:19 -0700

    Update validator.js

--- art4/validator.js
+++ art4/validator.js
@@ -1,38 +1,20 @@
-function isValid() {
-    if (firstName() //&&
-       // lastName()
-    )
-    return true;
-    else
-        document.getElementById("submiterror").innerHTML = "<p><strong>Error Submitting — See Above</strong></p>";
-        event.preventDefault();
-        return false;
-}
+<script  type="text/javascript">
+ var frmvalidator = new Validator("myform");
+ frmvalidator.addValidation("FirstName","req","Please enter your First Name");
+ frmvalidator.addValidation("FirstName","maxlen=20",
+		"Max length for FirstName is 20");
 
-FirstName.addEventListener('blur', firstName, false);
-function firstName(){
-    //1) Create variable
-    var validFirstname=false;
+ frmvalidator.addValidation("LastName","req");
+ frmvalidator.addValidation("LastName","maxlen=50");
 
-    //2) read value from HTML
-    var firstname = document.getElementById("FirstName").value;
-    var errorMessages = "";
+ frmvalidator.addValidation("Email","maxlen=50");
+ frmvalidator.addValidation("Email","req");
+ frmvalidator.addValidation("Email","email");
 
-    //3) Do validation
-    if (firstname==="null" || firstname==="" || firstname.length > 20 ) {
-        errorMessages += "<p>The first name is required and cannot be greater than 20 characters</p>";
-        console.log("First name invalid — length")
-        } else if (firstname.match("^[a-zA-Z ,.'-]+$")===null) {
-            errorMessages += "<p>Invalid caracter in last name (accepts only A-Z, a-z, and ,.'-)</p>";
-            console.log("First name invalid — bad characters")
-        } else {
-                validFirstname = true;
-                console.log("First name valid")
-        };
+ frmvalidator.addValidation("Phone","maxlen=15");
+ frmvalidator.addValidation("Phone","numeric");
 
-    //4) Send error message to HTML
-    document.getElementById("fname").innerHTML = errorMessages;
+ frmvalidator.addValidation("Address","maxlen=50");
+ frmvalidator.addValidation("Country","dontselect=000");
 
-    //5) return status of each field
-    return (validFirstname);
-};
+</script>

a819d06c14803aeabd911dc8f1593a3a939359e5
Isadora Silva <110069448+euaisa@users.noreply.github.com>
2022-11-04 11:37:24 -0700

    Update validator.js

--- art4/validator.js
+++ art4/validator.js
@@ -1,7 +1,8 @@
 function validateForm() {
 
-var firstname = document.getElementById("FirstName").value;
+var errorMessages = []
 
+var firstname = document.getElementById("FirstName").value;
 if (document.myForm.firstname.value ==="null" || firstname==="" || firstname.length > 20) {
             errorMessages += "<p>The first name is required and cannot be greater than 20 characters</p>";
             document.myForm.firstname.focus() ;
@@ -9,112 +10,91 @@ if (document.myForm.firstname.value ==="null" || firstname==="" || firstname.len
 }
 
 
-
-var validLastname = false;
 var lastname = document.getElementById("LastName").value;
-if (lastname==="null" || lastname==="" || lastname.length > 50)
-    errorMessages += "<p>The last name is required and cannot be greater than 50 characters</p>";
-else
-   validLastname = true;
-document.getElementById("errorMessages").innerHTML = errorMessages;
-return (validFirstname && validLastname);
+if (lastname==="null" || lastname==="" || lastname.length > 50) {
+            errorMessages += "<p>The last name is required and cannot be greater than 50 characters</p>";
+            document.myForm.lastname.focus() ;
+            return false;
+}
 
 
-validEmail = false;
 var userEmail = document.getElementById("Email").value;
 var atpos = userEmail.indexOf("@");
 var dotpos = userEmail.lastIndexOf(".");
-if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=userEmail.length)
-    errorMessages += "<p>Invalid email</p>";
-else
-   validEmail = true;
-document.getElementById("Email").innerHTML = errorMessages;
-return (validFirstname && validLastname && validEmail);
+if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=userEmail.length) {
+            errorMessages += "<p>Invalid email</p>";
+            document.myForm.userEmail.focus() ;
+            return false;
+}
 
 
-validPhone = false;
 var phone = document.getElementById("Phone").value;
-if (isNaN(phone) || phone.lenght >15 || phone===null || phone==="")
+if (isNaN(phone) || phone.lenght >15 || phone===null || phone==="") {
     errorMessages += "<p>Invalid phone number </p>";
-else
-  validPhone = true;
-document.getElementById("Phone").innerHTML = errorMessages;
-return (validFirstname && validLastname && validEmail && validPhone);
+    document.myForm.phone.focus() ;
+    return false;
+}
 
 
-
-validUsername = false;
 var Username = document.getElementById("Username").value;
-if (Username==="null" || Username==="" || Username.length > 12)
+if (Username==="null" || Username==="" || Username.length > 12) {
     errorMessages += "<p>The Username is required and cannot be greater than 12 characters</p>";
-else
-  validUsername = true;
-document.getElementById("Username").innerHTML = errorMessages;
-return (validFirstname && validLastname && validEmail && validPhone && validUsername);
+    document.myForm.Username.focus() ;
+    return false;
+}
 
 
-validPassword = false;
 var Password = document.getElementById("Password").value;
-if (Password==="null" || Password==="" || Password.length > 12)
+if (Password==="null" || Password==="" || Password.length > 12) {
     errorMessages += "<p>The Password is required and cannot be greater than 7 characters</p>";
-else
-  validPassword = true;
-document.getElementById("Password").innerHTML = errorMessages;
-return (validFirstname && validLastname && validEmail && validPhone && validUsername && validPassword);
+    document.myForm.Password.focus() ;
+    return false;
+}
 
 
-validAddress = false;
 var Address = document.getElementById("Address").value;
-if (Address==="null" || Address==="")
-    errorMessages += "<p>The Address is required</p>";
-else
-  validPassword = true;
-document.getElementById("Address").innerHTML = errorMessages;
-return (validFirstname && validLastname && validEmail && validPhone && validUsername && validPassword && validAddress);
+if (Address==="null" || Address==="") {
+            errorMessages += "<p>The Address is required</p>";
+            document.myForm.Address.focus() ;
+            return false;
+}
 
 
-validCity = false;
 var City = document.getElementById("City").value;
-if (City==="null" || City==="")
+if (City==="null" || City==="") {
     errorMessages += "<p>The City is required</p>";
-else
-  validCity = true;
-document.getElementById("City").innerHTML = errorMessages;
-return (validFirstname && validLastname && validEmail && validPhone && validUsername && validPassword && validAddress && validCity);
+    document.myForm.City.focus() ;
+    return false;
+}
 
 
 
-validState = false;
 var State = document.getElementById("State").value;
-if (State==="null" || State==="")
-    errorMessages += "<p>The State is required</p>";
-else
-  validState = true;
-document.getElementById("State").innerHTML = errorMessages;
-return (validFirstname && validLastname && validEmail && validPhone && validUsername && validPassword && validAddress && validCity && validState);
+if (State==="null" || State==="") {
+            errorMessages += "<p>The State is required</p>";
+            document.myForm.State.focus() ;
+            return false;
+}
 
 
-validCountry = false;
+
 var Country = document.getElementById("Country").value;
-if (Country==="null" || Country==="")
-    errorMessages += "<p>The Country is required</p>";
-else
-  validCountry = true;
-document.getElementById("Country").innerHTML = errorMessages;
-return (validFirstname && validLastname && validEmail && validPhone && validUsername && validPassword
-        && validAddress && validCity && validState && validCountry);
+if (Country==="null" || Country==="") {
+            errorMessages += "<p>The Country is required</p>";
+            document.myForm.Country.focus() ;
+            return false;
+}
 
 
 
-validZipCode = false;
 var ZipCode = document.getElementById("ZipCode").value;
-if (ZipCode==="null" || ZipCode==="" || ZipCode.length > 5)
-    errorMessages += "<p>The ZipCode is required and cannot be greater than 5 characters</p>";
-else
-  validPassword = true;
-document.getElementById("ZipCode").innerHTML = errorMessages;
-return (validFirstname && validLastname && validEmail && validPhone && validUsername && validPassword
-        && validAddress && validCity && validState && validCountry && validZipCode );
+if (ZipCode==="null" || ZipCode==="" || ZipCode.length > 5) {
+            errorMessages += "<p>The ZipCode is required and cannot be greater than 5 characters</p>";
+            document.myForm.ZipCode.focus() ;
+            return false;
+}
+return (true);
+}
 
 }
 

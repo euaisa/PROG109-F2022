3d7593a15482d8d7f015fdb4ba4cdf090e03b038
Isadora Silva <110069448+euaisa@users.noreply.github.com>
2022-11-03 21:47:37 -0700

    Update validator.js

--- art4/validator.js
+++ art4/validator.js
@@ -4,12 +4,11 @@
  frmvalidator.addValidation("FirstName","maxlen=20",
 		"Max length for FirstName is 20");
 
- frmvalidator.addValidation("LastName","req");
+ frmvalidator.addValidation("LastName","req", "Please enter your Last Name");
  frmvalidator.addValidation("LastName","maxlen=50");
 
  frmvalidator.addValidation("Email","maxlen=50");
- frmvalidator.addValidation("Email","req");
- frmvalidator.addValidation("Email","email");
+ frmvalidator.addValidation("Email","req", "Please enter your Email");
 
  frmvalidator.addValidation("Phone","maxlen=15");
  frmvalidator.addValidation("Phone","numeric");

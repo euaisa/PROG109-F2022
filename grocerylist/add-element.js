ef8db437d43e4680d36457156b974df42e38f86b
Isadora Silva <110069448+euaisa@users.noreply.github.com>
2022-11-12 18:26:02 -0800

    Update add-element.js

--- grocerylist/add-element.js
+++ grocerylist/add-element.js
@@ -6,3 +6,11 @@ newEl.appendChild(newText);
 var position = document.getElementsByTagName('ul')[0];
 position.appendChild(newEl);
 }
+
+var btn = document.getElementById('btnAddElement');
+btn.addEventListener('click', function handleClick(event) {
+  event.preventDefault();
+  var newInput = document.getElementById('items');
+  console.log(newInput.value);
+  newInput.value = '';
+});

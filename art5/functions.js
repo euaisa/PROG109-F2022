2eb0332a89d7f1c0e4fc03894b7feaee7cd1df70
Isadora Silva <110069448+euaisa@users.noreply.github.com>
2022-11-20 11:46:41 -0800

    Update functions.js

--- art5/functions.js
+++ art5/functions.js
@@ -35,6 +35,6 @@ nextButton.addEventListener("click",next,false);
 
 function autoSlide(){
     if (document.getElementById("auto").checked)
-      setInterval(autoSlide,2000);
+      setTimeout(autoSlide,2000);
       next();
 }
